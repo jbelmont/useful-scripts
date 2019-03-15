d48979b55537b240e25003f2f5999a3a34a25a9d
Jean-Marcel Belmont <jbelmont@foodlogiq.com>
2019-03-15 10:53:39 -0400

    Slight modification to convert clipboard script.

--- convert-clipboard-to-gitbook-formatting.sh
+++ convert-clipboard-to-gitbook-formatting.sh
@@ -6,7 +6,7 @@ readarray -t y <<< "$WORDS" 2> /dev/null
 
 for word in $WORDS
 do
-  TEXT_INSIDE_PARENS=$(echo $word | sed 's/ /_/g' | tr '[:upper:]' '[:lower:]')
+  TEXT_INSIDE_PARENS=$(echo $word | sed 's/ /-/g' | tr '[:upper:]' '[:lower:]')
   printf "* [%s](#%s)\n" $word $TEXT_INSIDE_PARENS
 done
 
